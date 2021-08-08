c119e94b1730ca9f02ec109af19d42102c0af6c9
hatsu38 <hajiwata0308@gmail.com>
2021-08-08 17:29:04 +0900

    Add HTTP Reuqest

--- main.go
+++ main.go
@@ -1,20 +1,68 @@
 package main
 
 import (
+	"net/http"
+	"fmt"
+	"os"
+	"io"
+
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
-
 func main() {
-	// Flex
+
 	app := tview.NewApplication()
-	flex := tview.NewFlex().
-		AddItem(tview.NewBox().SetBorder(true).SetTitle("HTTP METHODS"), 0, 1, true).
-		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
-			AddItem(tview.NewBox().SetBorder(true).SetTitle("URL"), 0, 1, false).
-			AddItem(tview.NewBox().SetBorder(true).SetTitle("Params"), 0, 2, false).
-			AddItem(tview.NewBox().SetBorder(true).SetTitle("Response"), 0, 4, false), 0, 4, false)
-	if err := app.SetRoot(flex, true).SetFocus(flex).Run(); err != nil {
+
+	inputField := tview.NewInputField()
+	inputField.SetLabel("Request URL: ")
+	inputField.SetPlaceholder("https://httpbin.org/get")
+	inputField.SetTitle("URL")
+	inputField.SetFieldTextColor(tcell.ColorMaroon)
+	inputField.SetLabelColor(tcell.ColorBlue)
+	inputField.SetFieldBackgroundColor(tcell.ColorGray)
+	inputField.SetPlaceholderTextColor(tcell.ColorWhite)
+	inputField.SetBorder(true)
+
+	textView := tview.NewTextView()
+	textView.SetTitle("Response")
+	textView.SetBorder(true)
+
+	inputField.SetChangedFunc(func(text string) {
+		textView.SetText(text)
+	})
+	inputField.SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEnter {
+			text := textView.GetText(true)
+			req, _ := http.NewRequest("GET", text, nil)
+			client := new(http.Client)
+
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				app.Stop()
+				os.Exit(1)
+			}
+			defer resp.Body.Close()
+
+			body, respErr := io.ReadAll(resp.Body)
+			if respErr != nil {
+				fmt.Fprintln(os.Stderr, err)
+				app.Stop()
+				os.Exit(1)
+			}
+			toFixBody := "{" + string(body) + "}}"
+			textView.SetText(toFixBody)
+		}
+	})
+
+
+	flex := tview.NewFlex()
+	flex.SetDirection(tview.FlexRow).
+		AddItem(inputField, 3, 0, true).
+		AddItem(textView, 0, 1, false)
+
+	if err := app.SetRoot(flex, true).Run(); err != nil {
 		panic(err)
 	}
 }
