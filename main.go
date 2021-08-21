fe72806882a620b1b5f14dd5aee57ec2c20f262d
hatsu38 <hajiwata0308@gmail.com>
2021-08-21 17:24:06 +0900

    Add flag

--- main.go
+++ main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"log"
+	"flag"
+	"fmt"
+	"os"
 
 	"github.com/hatsu38/postman-tui/ui"
 )
 
+func usage() {
+	format := `
+	                 _                               _         _
+	_ __   ___  ___| |_ _ __ ___   __ _ _ __       | |_ _   _(_)
+       | '_ \ / _ \/ __| __| '_ ' _ \ / _' | '_ \ _____| __| | | | |
+       | |_) | (_) \__ \ |_| | | | | | (_| | | | |_____| |_| |_| | |
+       | .__/ \___/|___/\__|_| |_| |_|\__,_|_| |_|      \__|\__,_|_|
+       |_|
+
+	Usage:
+	  postman-tui
+	Author:
+	  hatsu38<hajiwata0308@gmail.com>
+	`
+	fmt.Fprintln(os.Stderr, fmt.Sprintf(format))
+}
 func run() int {
 	var i interface{}
 	if err := ui.New().Run(i); err != nil {
@@ -14,6 +33,10 @@ func run() int {
 	}
 	return 0
 }
+
 func main() {
+	flag.Usage = usage
+	flag.Parse()
+
 	run()
 }
