a50669696a32e5d16a64fc5aa0a15ee2d8381f15
hatsu38 <hajiwata0308@gmail.com>
2021-08-22 16:10:32 +0900

    Add version

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 	"github.com/hatsu38/postman-tui/ui"
 )
 
+var (
+	Version  string
+	Revision string
+)
+
 func usage() {
 	format := `
 	                 _                               _         _
@@ -16,15 +21,16 @@ func usage() {
        | '_ \ / _ \/ __| __| '_ ' _ \ / _' | '_ \ _____| __| | | | |
        | |_) | (_) \__ \ |_| | | | | | (_| | | | |_____| |_| |_| | |
        | .__/ \___/|___/\__|_| |_| |_|\__,_|_| |_|      \__|\__,_|_|
-       |_|
+       |_|  Version: %s-%s
 
 	Usage:
 	  postman-tui
 	Author:
 	  hatsu38<hajiwata0308@gmail.com>
 	`
-	fmt.Fprintln(os.Stderr, fmt.Sprintf(format))
+	fmt.Fprintln(os.Stderr, fmt.Sprintf(format, Version, Revision))
 }
+
 func run() int {
 	var i interface{}
 	if err := ui.New().Run(i); err != nil {
