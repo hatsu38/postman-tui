489744f7a62c7e94ba5d5c670168a2d718a65288
hatsu38 <hajiwata0308@gmail.com>
2021-08-22 18:18:10 +0900

    Add version flag

--- main.go
+++ main.go
@@ -16,18 +16,22 @@ var (
 
 func usage() {
 	format := `
-	                 _                               _         _
-	_ __   ___  ___| |_ _ __ ___   __ _ _ __       | |_ _   _(_)
-       | '_ \ / _ \/ __| __| '_ ' _ \ / _' | '_ \ _____| __| | | | |
-       | |_) | (_) \__ \ |_| | | | | | (_| | | | |_____| |_| |_| | |
-       | .__/ \___/|___/\__|_| |_| |_|\__,_|_| |_|      \__|\__,_|_|
-       |_|  Version: %s-%s
+		_                               _         _
+_ __   ___  ___| |_ _ __ ___   __ _ _ __       | |_ _   _(_)
+| '_ \ / _ \/ __| __| '_ ' _ \ / _' | '_ \ _____| __| | | | |
+| |_) | (_) \__ \ |_| | | | | | (_| | | | |_____| |_| |_| | |
+| .__/ \___/|___/\__|_| |_| |_|\__,_|_| |_|      \__|\__,_|_|
+|_|  Version: %s-%s
 
-	Usage:
-	  postman-tui
-	Author:
-	  hatsu38<hajiwata0308@gmail.com>
-	`
+Usage:
+  postman-tui
+
+  Flags:
+    -version: Print the current version
+
+Author:
+  hatsu38<hajiwata0308@gmail.com>
+`
 	fmt.Fprintln(os.Stderr, fmt.Sprintf(format, Version, Revision))
 }
 
@@ -41,8 +45,16 @@ func run() int {
 }
 
 func main() {
+	var withVersion bool
+	flag.BoolVar(&withVersion, "version", false, "Print the current version")
+
 	flag.Usage = usage
 	flag.Parse()
+
+	if withVersion {
+		fmt.Println("Version ", Version)
+		return
+	}
 
 	run()
 }
